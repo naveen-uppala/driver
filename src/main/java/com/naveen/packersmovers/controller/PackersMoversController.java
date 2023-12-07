706f26a7a83335df80c4e2333f3f08d16775ee47
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2023-12-07 07:59:04 +0530

    Update PackersMoversController.java

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -30,7 +30,7 @@ public class PackersMoversController {
         
         
         @GetMapping("/ping")
-        public String ping() {  return "Hi, this is Alekhya" ;
+        public String ping() {  return "Hi, this is Delip" ;
         			
         }
         
