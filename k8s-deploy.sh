c334390008329c6eae01e182030b05b327beb35d
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2024-02-13 23:04:01 +0530

    Update k8s-deploy.sh

--- k8s-deploy.sh
+++ k8s-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-cd /K8s-Deployment
+cd K8s-Deployment
 kustomize edit set image 533266968694.dkr.ecr.ap-south-1.amazonaws.com/customer:default_tag=${REPOSITORY_URI} && kubectl apply -k .
