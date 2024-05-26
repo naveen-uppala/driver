cca71a87d42a6fb8cdaa66a254e037a5f0072b59
SandeepKomal <sandeepkomalp@gmail.com>
2024-05-26 15:11:58 +0530

    Update PackersMoversController.java

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -10,24 +10,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/customer")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-
 public class PackersMoversController {
-        @Autowired
-        private PackersMoversService userService;
-                
-        @Autowired
-        private MessageService messageService;
 
-       
-        @PostMapping("/customerDetails")
-        public String customerDetails(@RequestBody Customer customer){
-            userService.saveUser(customer);
-            //messageService.sendMessage(customer);
-            return "New user is added";
-        }
-        
-        
-        @GetMapping("/ping")
-        public String ping() {  return "Hi this is Sandeep";}             
+    @Autowired
+    private PackersMoversService userService;
 
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/customerDetails")
+    public String customerDetails(@RequestBody Customer customer) {
+        userService.saveUser(customer);
+        //messageService.sendMessage(customer);
+        return "New user is added";
     }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Hi this is Sandeep";
+    }
+}
+
