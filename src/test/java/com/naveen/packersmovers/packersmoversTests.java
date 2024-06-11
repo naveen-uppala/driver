1a9804ee22d126169eee67e13c8b414a72e45e8b
SandeepKomal <sandeepkomalp@gmail.com>
2024-06-11 23:04:54 +0530

    Update packersmoversTests.java

--- src/test/java/com/naveen/packersmovers/packersmoversTests.java
+++ src/test/java/com/naveen/packersmovers/packersmoversTests.java
@@ -1,13 +1,21 @@
 package com.naveen.packersmovers;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class packersmoversTests {
+public class packersmoversTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
+
+
+
+
