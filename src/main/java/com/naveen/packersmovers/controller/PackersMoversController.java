c31f6a0a1cb4032d8989fa8495ffe7ea5cf8ce0c
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2024-10-23 15:48:30 +0530

    Update PackersMoversController.java

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -1,7 +1,7 @@
 package com.naveen.packersmovers.controller;
+
 import com.naveen.packersmovers.model.Customer;
 import com.naveen.packersmovers.service.PackersMoversService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,18 +10,25 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 
 public class PackersMoversController {
-        @Autowired
-        private PackersMoversService userService;
-                
-       
-        @PostMapping("/customerDetails")
-        public String customerDetails(@RequestBody Customer customer){
-            userService.saveUser(customer);
-            return "New user is added";
-        }
-        
-        
-        @GetMapping("/ping")
-        public String ping() {  return "Hi, this is v2 version";}             
 
+    @Autowired
+    private PackersMoversService userService;
+
+    @PostMapping("/customerDetails")
+    public String customerDetails(@RequestBody Customer customer) {
+        userService.saveUser(customer);
+        return "New user is added";
     }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Hi, this is v2 version";
+    }
+
+    // Vulnerable endpoint to XSS
+    @GetMapping("/greet")
+    public String greet(@RequestParam("name") String name) {
+        // Directly returning user input without validation (XSS Vulnerability)
+        return "Hello, " + name;
+    }
+}
